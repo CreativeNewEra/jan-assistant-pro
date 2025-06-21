6f028112c2e938a550448520e4224bb6b13fa017
CreativeNewEra <149348461+CreativeNewEra@users.noreply.github.com>
2025-06-21 14:54:36 -0400

    Add AsyncGUIBridge for GUI async tasks

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -1,3 +1,4 @@
+from src.gui.async_bridge import AsyncGUIBridge
 from src.gui.enhanced_widgets import ChatInput, EnhancedChatDisplay, StatusBar
 
-__all__ = ["StatusBar", "ChatInput", "EnhancedChatDisplay"]
+__all__ = ["StatusBar", "ChatInput", "EnhancedChatDisplay", "AsyncGUIBridge"]
